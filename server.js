5d27a103b684249696530b17395f014db3b13156
Paulina Ruta <paulina.ruta@outlook.com>
2023-09-24 13:35:02 +0200

    Add frontend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const uuid = require('uuid');
 const router = express.Router();
+const cors = require('cors');
 
 const app = express();
 
@@ -10,6 +11,7 @@ const seatsRoutes = require('./routes/seats.routes');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/', testimonialsRoutes);
 app.use('/api/', concertsRoutes);
